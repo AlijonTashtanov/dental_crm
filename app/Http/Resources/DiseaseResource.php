2d3910fcbd8585bdeb52f16fa0984e3d2f65528e
Muhammadali <muhammadaliwebdeveloper@gmail.com>
2024-02-05 13:06:42 +0500

    disaases and patiend crud finished

--- app/Http/Resources/DiseaseResource.php
+++ app/Http/Resources/DiseaseResource.php
@@ -15,7 +15,8 @@ class DiseaseResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->name,
+            'id' => $this->id,
+            'name' => $this->name,
             'color' => $this->color
         ];
     }
